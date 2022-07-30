33499659d39e31caa6ccb541aa32b5bfc50595ac
amichno <95045327+amichno@users.noreply.github.com>
2022-07-30 12:15:29 +0200

    add input

---
+++ src/components/TextInput.component.tsx
@@ -0,0 +1,14 @@
+
+export interface TextInputProps{
+    text:string;
+}
+
+const TextInput: React.FC<TextInputProps> = ({text})=>{
+    return(
+        <div>
+            <input value={text}/>
+        </div>
+    )
+}
+
+export default TextInput;
